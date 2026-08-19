af1a0507a15d20f9449093b3ef6959cc6200bf29
Darek <jonskid44@gmail.com>
2026-08-19 19:11:39 +0200

    Updated pipeline to support other commodities

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ from loader import load_data_to_db
 def run_pipeline():
     print("--- Starting ETL Pipeline ---")
     
-    raw_data = fetch_currency_data("Allflame")
-    if raw_data is None:
-        print("Pipeline aborted: Extraction failed.")
-        return
+    categories = ["Currency", "Fragment", "Scarab", "DivinationCard"]
+    for category in categories:
+        raw_data = fetch_currency_data("Allflame", category)
+        if raw_data is None:
+            print("Pipeline aborted: Extraction failed.")
+            return
+            
+        clean_df = clean_currency_data(raw_data)
+        if clean_df is None or clean_df.empty:
+            print("Pipeline aborted: Transformation resulted in empty data.")
+            return
+            
+        load_data_to_db(clean_df)
         
-    clean_df = clean_currency_data(raw_data)
-    if clean_df is None or clean_df.empty:
-        print("Pipeline aborted: Transformation resulted in empty data.")
-        return
-        
-    load_data_to_db(clean_df)
-    
     print("--- ETL Pipeline Completed Successfully ---")
 
 if __name__ == "__main__":
